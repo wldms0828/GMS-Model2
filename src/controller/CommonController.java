451ed9f43a148bdd5cafdc273d26de00be5b6c58
wldms0828 <whatsportskim@gmail.com>
2018-07-24 11:21:05 +0900

    session

--- src/controller/CommonController.java
+++ src/controller/CommonController.java
@@ -6,12 +6,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/common.do")
 public class CommonController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.setAttribute("ctx", request.getContextPath());
 		request.getRequestDispatcher("WEB-INF/view/main.jsp").forward(request, response);
 		
 	}
