b9768e90e5496ed2ef87a8f750c7b353423ba811
wldms0828 <whatsportskim@gmail.com>
2018-08-07 15:22:32 +0900

    proxy

--- src/proxy/PageProxy.java
+++ src/proxy/PageProxy.java
@@ -1,11 +1,26 @@
 package proxy;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import service.MemberServiceImpl;
+
 public class PageProxy implements Proxy {
+	//request에 set하는 거 까지 해준다.
+	//제네릭은 선언할 때만 쓴다.
+	HttpServletRequest request;
 	@Override
 	public Map<?, ?> carryOut(Map<?, ?> param) {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String, Object> map = new HashMap<>();
+		request=(HttpServletRequest) param.get("request");
+//		request.setAttribute("list", MemberServiceImpl.getInstance().getList(param));
+//		request.setAttribute("count", count);
+//		request.setAttribute("beginPage", beginPage);
+//		request.setAttribute("endPage", endPage );
+//		request.setAttribute("", endPage );
+		map.put("request", request);
+		return map;
 	}
 }
--- src/proxy/Pagination.java
+++ src/proxy/Pagination.java
@@ -1,5 +1,20 @@
 package proxy;
 
-public class Pagination {
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.Data;
+@Data
+public class Pagination implements Proxy {
+	String beginRow,endRow,pageIndex,pageIndex2;
+	int beginPage,endPage,pageSize,blockSize = 5;
+
+	@Override
+	public Map<?, ?> carryOut(Map<?, ?> param) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("beginRow", beginRow);
+		map.put("endRow", endRow);
+		return null;
+	}
 
 }
